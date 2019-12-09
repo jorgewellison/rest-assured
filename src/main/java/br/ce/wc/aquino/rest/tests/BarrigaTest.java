e68b5253eba590d9c09d1ac25c5c3f079aab5b75
jorgewellison <jorge.wellison@dcx.ufpb.br>
2019-12-09 15:50:00 -0300

    Deve alterar conta com sucesso

--- src/main/java/br/ce/wc/aquino/rest/tests/BarrigaTest.java
+++ src/main/java/br/ce/wc/aquino/rest/tests/BarrigaTest.java
@@ -1,15 +1,34 @@
 package br.ce.wc.aquino.rest.tests;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import br.ce.wcaquino.rest.core.BaseTest;;
 
 public class BarrigaTest extends BaseTest{
+	
+	private String TOKEN;
+	
+	@Before
+	public void login() {
+		Map<String, String> login = new HashMap<>();
+		login.put("email", "jorge.wellison@dcx.ufpb.br");
+		login.put("senha", "328406");
+		
+		TOKEN = given()
+			.body(login)
+		.when()
+			.post("/signin")
+		.then()
+			.statusCode(200)
+			.extract().path("token");
+	}
 	
 	@Test
 	public void naoDeveAcessarAPISemToken() {
@@ -23,24 +42,26 @@ public class BarrigaTest extends BaseTest{
 	
 	@Test
 	public void deveIncluirContaComSucesso() {
-		Map<String, String> login = new HashMap<>();
-		login.put("email", "jorge.wellison@dcx.ufpb.br");
-		login.put("senha", "328406");
-		
-		String token = given()
-			.body(login)
-		.when()
-			.post("/signin")
-		.then()
-			.statusCode(200)
-			.extract().path("token");
-		
 		given()
-			.header("Authorization", "JWT " + token)
+			.header("Authorization", "JWT " + TOKEN)
 			.body("{ \"nome\": \"conta qualquer\" }")	
 		.when()
 			.post("/contas")
 		.then()
 			.statusCode(201);
+	}
+	
+	@Test
+	public void deveAlterarContaComSucesso() {
+
+		given()
+			.header("Authorization", "JWT " + TOKEN)
+			.body("{ \"nome\": \"conta alterada\" }")	
+		.when()
+			.put("/contas/46139")
+		.then()
+			.log().all()
+			.statusCode(200)
+			.body("nome", is("conta alterada"));
 	}
 }
